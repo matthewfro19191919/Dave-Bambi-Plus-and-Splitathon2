56b56bb9a8b0bd5c37d66faf0a710e94955fdd01
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-01-25 06:47:33 -0500

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -54,7 +54,7 @@ class Character extends FlxSprite
 		switch (curCharacter)
 		{
 			case 'bf':
-				frames = Paths.getSparrowAtlas('characters/BOYFRIEND', 'shared');
+				frames = Paths.getSparrowAtlas('characters/bf/BOYFRIEND', 'shared');
 				
 				animation.addByPrefix('idle', 'BF idle dance', 24, false);
 				animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
@@ -79,7 +79,7 @@ class Character extends FlxSprite
 				skins.set('gfSkin', 'gf');
 				skins.set('3d', 'bf-3d');
 
-				barColor = FlxColor.fromRGB(168, 120, 60);
+				barColor = FlxColor.fromRGB(49, 176, 209);
 
 				playAnim('idle');
 
@@ -89,7 +89,60 @@ class Character extends FlxSprite
 
 				setGraphicSize(Std.int((width * 3.5) / furiosityScale));
 
+			case 'eevee':
+				frames = Paths.getSparrowAtlas('characters/BOYFRIEND', 'shared');
+				
+				animation.addByPrefix('idle', 'BF idle dance', 24, false);
+				animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
+				animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
+				animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
+				animation.addByPrefix('singDOWN', 'BF NOTE DOWN0', 24, false);
+				animation.addByPrefix('singUPmiss', 'BF NOTE UP MISS', 24, false);
+				animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
+				animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
+				animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
+				animation.addByPrefix('hey', 'BF HEY', 24, false);
+
+				animation.addByPrefix('firstDeath', "BF dies", 24, false);
+				animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
+				animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+				animation.addByPrefix('dodge', "boyfriend dodge", 24, false);
+				animation.addByPrefix('scared', 'BF idle shaking', 24);
+				animation.addByPrefix('hit', 'BF hit', 24, false);
+
+				loadOffsetFile(curCharacter);
+
+				skins.set('gfSkin', 'gf');
+				skins.set('3d', 'eevee-3d');
+
+				barColor = FlxColor.fromRGB(168, 120, 60);
+
+				playAnim('idle');
+
+				nativelyPlayable = true;
+
+				flipX = true;
+
+				setGraphicSize(Std.int((width * 3.5) / furiosityScale));
 			case 'bf-3d':
+				frames = Paths.getSparrowAtlas('characters/bf/3d_bf', 'shared');
+
+				animation.addByPrefix('idle', 'idle', 24, false);
+				for (anim in ['left', 'down', 'up', 'right'])
+				{
+					animation.addByPrefix('sing${anim.toUpperCase()}', anim, 24, false);	
+				}
+				loadOffsetFile(curCharacter);
+				
+				globalOffset = [-85, -272];
+				barColor = FlxColor.fromRGB(49, 176, 209);
+
+				playAnim('idle');
+				antialiasing = false;
+				nativelyPlayable = true;
+				flipX = true;
+
+			case 'eevee-3d':
 				frames = Paths.getSparrowAtlas('characters/3d_bf', 'shared');
 
 				animation.addByPrefix('idle', 'idle', 24, false);
